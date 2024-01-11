fe0cc6e1a7e96409f8252244e37b1dd2c8f70cf9
samuelemarino04 <samuele12601@gmail.com>
2024-01-11 19:48:49 +0100

    needs font changing, remove padding sections

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,4 +1,6 @@
 import { Nav } from 'react-bootstrap'
+
+
 import './Navigation.css'
 
 
@@ -9,12 +11,17 @@ const Navigation = () => {
 
     return (
 
-        <Nav className="navbar navbar-expand-md navbar-light bg-dark">
-            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <Nav className="navbar navbar-expand-md  navbar-light bg-dark">
+            <div className="collapse navbar-collapse justify-content-start">
                 <ul class="navbar-nav">
                     <li class="nav-item"><a class="nav-link text-white" href="#sobre">About</a></li>
                     <li class="nav-item"><a class="nav-link text-white" href="#portfolio" >Projects</a></li>
                     <li class="nav-item"><a class="nav-link text-white" href="#encontrame">Contact me</a></li>
+                </ul>
+            </div>
+            <div className="justify-content-end">
+                <ul class="navbar-nav">
+                    <li class="nav-item text-white" style={{ marginRight: '15px' }}>Samuele Marino</li>
                 </ul>
             </div>
         </Nav>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -14,8 +14,8 @@ const Projects = () => {
                         <div class="jod-desc">
                             <h3>Videogame 2D</h3>
                             <p>Javascript, HTML, CSS</p>
-                            <Button variant="outline-success" href="https://github.com/samuelemarino04/LabVideoJuego">Repository</Button>
-                            <Button variant="outline-info" href="https://samuelemarino04.github.io/LabVideoJuego/">Web</Button>
+                            <Button variant="outline-light" href="https://github.com/samuelemarino04/LabVideoJuego" target="_blank" rel="noopener noreferrer">Repository</Button>
+                            <Button variant="outline-info" href="https://samuelemarino04.github.io/LabVideoJuego/" target="_blank" rel="noopener noreferrer">Web</Button>
                         </div>
                         <img src="public/images/2Dvideogame.png" class="img-fluid" />
                     </article>
@@ -23,18 +23,18 @@ const Projects = () => {
                     <article>
                         <div class="jod-desc">
                             <h3>Web page</h3>
-                            <p>Node.js, Express, HBS, MongoDB, Google places API</p>
-                            <Button variant="outline-success" href="https://github.com/samuelemarino04/M2Project">Repository</Button>
-                            <Button variant="outline-info" href="https://onlypizzas.fly.dev/">Web</Button>
+                            <p>Node.js, Express, HBS, MongoDB, Google places API, Edamam API</p>
+                            <Button variant="outline-light" href="https://github.com/samuelemarino04/M2Project" target="_blank" rel="noopener noreferrer">Repository</Button>
+                            <Button variant="outline-info" href="https://onlypizzas.fly.dev/" target="_blank" rel="noopener noreferrer">Web</Button>
                         </div>
                         <img src="public/images/onlypizza.png" class="img-fluid" />
                     </article>
                     <article>
                         <div class="jod-desc">
                             <h3>Single page application</h3>
-                            <p>React.js, node.js, CSS</p>
-                            <Button variant="outline-success" href="https://github.com/samuelemarino04/Q-support-client">Client Repository</Button>
-                            <Button variant="outline-success" href="https://github.com/samuelemarino04/Q-support-server">Server Repository</Button>
+                            <p>React.js, Node.js, CSS, Google places API, MongoDB</p>
+                            <Button variant="outline-light" href="https://github.com/samuelemarino04/Q-support-client" target="_blank" rel="noopener noreferrer">Client Repository</Button>
+                            <Button variant="outline-light" href="https://github.com/samuelemarino04/Q-support-server" target="_blank" rel="noopener noreferrer">Server Repository</Button>
                             <Button variant="outline-info" href="https://creativesqplus.netlify.app/">Web</Button>
                         </div>
                         <img src="public/images/singlepage.png" class="img-fluid" />
@@ -42,9 +42,9 @@ const Projects = () => {
                     <article>
                         <div class="jod-desc">
                             <h3>Devops Project</h3>
-                            <p>Azure, Docker, Terraform, Kubernetes, Prometheus, Grafana</p>
-                            <Button variant="outline-success" href="https://github.com/samuelemarino04/DevOpsProject">Repository</Button>
-                            <Button variant="outline-primary" href="https://www.canva.com/design/DAF3CMZI3lU/Mhe6wX5tqPkpjIXR96GpQw/view?utm_content=DAF3CMZI3lU&utm_campaign=designshare&utm_medium=link&utm_source=editor">Presentation</Button>
+                            <p>Azure, Docker, Ansible, Terraform, Kubernetes, Prometheus, Grafana</p>
+                            <Button variant="outline-light" href="https://github.com/samuelemarino04/DevOpsProject" target="_blank" rel="noopener noreferrer">Repository</Button>
+                            <Button variant="outline-primary" href="https://www.canva.com/design/DAF3CMZI3lU/Mhe6wX5tqPkpjIXR96GpQw/view?utm_content=DAF3CMZI3lU&utm_campaign=designshare&utm_medium=link&utm_source=editor" target="_blank" rel="noopener noreferrer">Presentation</Button>
                         </div>
                         <img src="public/images/Devops2.png" class="img-fluid" />
 
